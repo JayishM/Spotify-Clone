7879e5aa50f115e749cd8b907a9a8cc2dc768a2e
JayishM <jayishmundra@gmail.com>
2026-08-01 15:23:07 +0530

    5th commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+// Serve mp3 files
+app.use("/songs", express.static(path.join(__dirname, "songs")));
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("🎵 Spotify Backend Running!");
+});
+
+// API to return all songs
+app.get("/api/songs", (req, res) => {
+
+    const songsFolder = path.join(__dirname, "songs");
+
+    const files = fs
+        .readdirSync(songsFolder)
+        .filter(file => file.endsWith(".mp3"));
+
+    const songs = files.map((file, index) => ({
+        id: index + 1,
+        title: path.parse(file).name,
+        url: `http://localhost:${PORT}/songs/${encodeURIComponent(file)}`
+    }));
+
+    res.json(songs);
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
